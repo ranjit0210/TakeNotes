a3242c176df16f7d21ba5fa68de84aed8f7cf179
Keval Shukla <keval@akhilalabs.com>
2022-12-16 18:31:43 +0530

    - Implemented the code for showing the data from Room Database.

---
+++ app/src/main/java/activity/AddNoteActivity.java
@@ -0,0 +1,18 @@
+package activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hyunix.takenote.R;
+
+public class AddNoteActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
---
+++ app/src/main/java/com/hyunix/takenote/adapter/NoteAdapter.java
@@ -0,0 +1,64 @@
+package com.hyunix.takenote.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hyunix.takenote.R;
+import com.hyunix.takenote.entity.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder>
+{
+
+    private List<Note> notes = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
+    {
+        View view = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.note_item, parent, false);
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteViewHolder holder, int position)
+    {
+        Note currentNote = notes.get(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return notes.size();
+    }
+
+    public void setNotes(List<Note> notes)
+    {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    class NoteViewHolder extends RecyclerView.ViewHolder
+    {
+        TextView textViewTitle, textViewDescription;
+
+        public NoteViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.txtViewTitle);
+            textViewDescription = itemView.findViewById(R.id.txtViewDescription);
+        }
+    }
+}
