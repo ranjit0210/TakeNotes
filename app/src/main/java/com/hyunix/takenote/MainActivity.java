92111c3c9724217a9f628b86a7cc4580db5e375d
Keval Shukla <keval@akhilalabs.com>
2022-12-16 19:07:58 +0530

    - Implemented the AddNoteActivity also added menu in toolbar in MainActivity

--- app/src/main/java/com/hyunix/takenote/MainActivity.java
+++ app/src/main/java/com/hyunix/takenote/MainActivity.java
@@ -1,12 +1,17 @@
 package com.hyunix.takenote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.hyunix.takenote.activity.AddNoteActivity;
 import com.hyunix.takenote.adapter.NoteAdapter;
 import com.hyunix.takenote.viewmodel.NoteViewModel;
 
@@ -30,5 +35,30 @@ public class MainActivity extends AppCompatActivity
                 .create(NoteViewModel.class);
 
         noteViewModel.getAllNote().observe(this, notes -> adapter.setNotes(notes));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+
+        getMenuInflater().inflate(R.menu.new_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+
+        switch (item.getItemId())
+        {
+            case R.id.top_menu:
+                Intent i = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
