2bcf380a718d10f4e7796f52a9c74e96ba7c3092
Keval Shukla <keval@akhilalabs.com>
2022-12-16 16:46:38 +0530

    - Added the Repository, ViewModel class.

--- app/src/main/java/com/hyunix/takenote/MainActivity.java
+++ app/src/main/java/com/hyunix/takenote/MainActivity.java
@@ -1,16 +1,37 @@
 package com.hyunix.takenote;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.hyunix.takenote.entity.Note;
+import com.hyunix.takenote.viewmodel.NoteViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider.AndroidViewModelFactory(getApplication())
+                .create(NoteViewModel.class);
+
+        noteViewModel.getAllNote().observe(this, new Observer<List<Note>>()
+        {
+            @Override
+            public void onChanged(List<Note> notes)
+            {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/hyunix/takenote/databse/NoteDatabase.java
+++ app/src/main/java/com/hyunix/takenote/databse/NoteDatabase.java
@@ -1,10 +1,19 @@
 package com.hyunix.takenote.databse;
 
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.hyunix.takenote.entity.Note;
 import com.hyunix.takenote.entity.NoteDao;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase
@@ -12,4 +21,66 @@ public abstract class NoteDatabase extends RoomDatabase
     private static NoteDatabase instance;
 
     public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context)
+    {
+
+        if (instance == null)
+        {
+            instance = Room.databaseBuilder(context.getApplicationContext(), NoteDatabase.class, "note_database")
+                    .fallbackToDestructiveMigration()
+                    //.allowMainThreadQueries() --> not recommended
+                    .addCallback(roomCallback)
+                    .build();
+        }
+        return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback()
+    {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db)
+        {
+            super.onCreate(db);
+
+            NoteDao noteDao = instance.noteDao();
+
+            ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+            executorService.execute(() -> {
+                noteDao.insert(new Note("Title 1", "Description1"));
+                noteDao.insert(new Note("Title 2", "Description2"));
+                noteDao.insert(new Note("Title 3", "Description3"));
+                noteDao.insert(new Note("Title 4", "Description4"));
+                noteDao.insert(new Note("Title 5", "Description5"));
+                noteDao.insert(new Note("Title 6", "Description6"));
+            });
+
+//            new PopulateAsyncTask(instance).execute();
+
+        }
+    };
+/*
+    private static class PopulateAsyncTask extends AsyncTask<Void, Void, Void>
+    {
+        public PopulateAsyncTask(NoteDatabase noteDatabase)
+        {
+            this.noteDao = noteDatabase.noteDao();
+        }
+
+        private NoteDao noteDao;
+
+        @Override
+        protected Void doInBackground(Void... voids)
+        {
+
+            noteDao.insert(new Note("Title 1", "Description1"));
+            noteDao.insert(new Note("Title 2", "Description2"));
+            noteDao.insert(new Note("Title 3", "Description3"));
+            noteDao.insert(new Note("Title 4", "Description4"));
+            noteDao.insert(new Note("Title 5", "Description5"));
+            noteDao.insert(new Note("Title 6", "Description6"));
+            return null;
+        }
+    }*/
 }
