47b77e0521da6e0f7a22a85c4a0e77cb3002020e
Keval Shukla <keval@akhilalabs.com>
2022-12-19 12:54:12 +0530

    - Implemented the feature of insert the new notes in database.

--- app/src/main/java/com/hyunix/takenote/MainActivity.java
+++ app/src/main/java/com/hyunix/takenote/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.hyunix.takenote.activity.AddNoteActivity;
 import com.hyunix.takenote.adapter.NoteAdapter;
+import com.hyunix.takenote.entity.Note;
 import com.hyunix.takenote.viewmodel.NoteViewModel;
 
 public class MainActivity extends AppCompatActivity
@@ -21,11 +24,16 @@ public class MainActivity extends AppCompatActivity
     private NoteViewModel noteViewModel;
     private NoteAdapter adapter = new NoteAdapter();
 
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // to register activity
+        setActivityResultLauncher();
 
         RecyclerView recyclerView = findViewById(R.id.recyclerList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -54,11 +62,53 @@ public class MainActivity extends AppCompatActivity
         {
             case R.id.top_menu:
                 Intent i = new Intent(MainActivity.this, AddNoteActivity.class);
-                startActivity(i);
+                // startActivityForResult(i, 1); // deprecated function logic
+
+                // ActivityResultLauncher current work on it
+                activityResultLauncher.launch(i);
+
+                //activityResultLauncher
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
 
     }
+
+    public void setActivityResultLauncher()
+    {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+
+            int resultCode = result.getResultCode();
+
+            Intent data = result.getData();
+
+            if (resultCode == RESULT_OK && data != null)
+            {
+                String title = data.getStringExtra("noteTitle");
+                String description = data.getStringExtra("noteDescription");
+
+                Note note = new Note(title, description);
+
+                noteViewModel.insert(note);
+            }
+        });
+    }
+/*
+
+// Deprecated method code
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null)
+        {
+            String title = data.getStringExtra("noteTitle");
+            String description = data.getStringExtra("noteDescription");
+
+            Note note = new Note(title, description);
+            noteViewModel.insert(note);
+        }
+    }*/
 }
